0a903ba706ef354049b2c93ab6366b082fdc9d13
pengyu965 <py.yan965@gmail.com>
2019-07-23 10:38:19 -0400

    update

--- data_process/verify_etc.py
+++ data_process/verify_etc.py
@@ -7,13 +7,14 @@ img_path = "../data/SUMIT/rs_images_sampled/"
 
 gt_list = os.listdir(gt_path)
 img_list = os.listdir(img_path)
+print(len(gt_list), len(img_list))
 
-for file in gt_list:
-    img = file[:-4]+"png"
-    if img not in img_list:
-        print(img)
+# for file in gt_list:
+#     img = file[:-4]+"png"
+#     if img not in img_list:
+#         print(img)
 
-for img in img_list:
-    gt = file[:-3]+"json"
-    if gt not in gt_list:
-        print(gt_list)
+# for img in img_list:
+#     gt = file[:-3]+"json"
+#     if gt not in gt_list:
+#         print(gt_list)

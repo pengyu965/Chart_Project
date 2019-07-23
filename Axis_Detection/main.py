49014a9d3832fbdaecff02b3cc372d99c38379ec
pengyu965 <py.yan965@gmail.com>
2019-07-23 14:38:26 -0400

    update

--- Axis_Detection/main.py
+++ Axis_Detection/main.py
@@ -221,6 +221,7 @@ if cfg.predict:
 
         iimg = cv2.imread(img_path+dataset[idi])
         for box in boxes_list[0]:
+            print(box)
             cv2.rectangle(iimg, (box[0],box[1]), (box[2], box[3]), (0,0,255), 2)
         
         cv2.imwrite("./samples/"+dataset[idi])

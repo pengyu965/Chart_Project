100818db55fae7cbacb70ee31e7ec8deac024f30
pengyu965 <py.yan965@gmail.com>
2019-07-25 13:37:04 -0400

    update

--- Pytorch_DCGAN_ticks_mask/main.py
+++ Pytorch_DCGAN_ticks_mask/main.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
 
         if os.path.exists("./weight/model.pt"):
             if torch.cuda.is_available():
-                netG.load_state_dict(torch.load("./weight/model.pt"), strict=False)
+                netG.load_state_dict(torch.load("./weight/model.pt"), strict=True)
             else:
                 netG.load_state_dict(torch.load("./weight/model.pt", map_location='cpu'), strict=True)
             print("="*6, "\nModel loaded, start prediction", "\n"+"="*6)
--- Pytorch_DCGAN_ticks_mask/op.py
+++ Pytorch_DCGAN_ticks_mask/op.py
@@ -151,7 +151,10 @@ class Operator:
             image_name,_ = os.path.splitext(os.path.split(image_path)[1])
             print("image_path is a image file")
             img_tensor = torch.tensor(
-                    cv2.imread(image_path)
+                cv2.resize(
+                    cv2.imread(image_path),
+                    (512,512)
+                )
                 ).permute(2,0,1).float().unsqueeze(0).to(self.device)
             generated_img_tensor = self.netG(img_tensor)
             generated_img = Image.fromarray(

ebee7d66ae25d404bc1101e3a3594f5087fecdd3
pengyu965 <py.yan965@gmail.com>
2019-07-29 16:13:10 -0400

    update

--- Pytorch_DCGAN_ticks_mask/main.py
+++ Pytorch_DCGAN_ticks_mask/main.py
@@ -55,6 +55,16 @@ if __name__ == "__main__":
     if FLAGS.train:
         netG = UNet(in_channels = 3, out_channels = 6)
         print(netG)
+        
+        if os.path.exists("./weight/model.pt"):
+            if torch.cuda.is_available():
+                netG.load_state_dict(torch.load("./weight/model.pt"), strict=True)
+            else:
+                netG.load_state_dict(torch.load("./weight/model.pt", map_location='cpu'), strict=True)
+            print("="*6, "\nModel loaded, start prediction", "\n"+"="*6)
+        else:
+            print("Model isn't found, train the network first.")
+            sys.exit()
 
         operator = op.Operator(netG)
         operator.trainer(FLAGS.img_path, FLAGS.gt_path, FLAGS.bsize, FLAGS.lr, FLAGS.epoch)

d7f2183e081bfbf509fb42a86cd783a50215534f
pengyu965 <py.yan965@gmail.com>
2019-08-01 19:47:09 -0400

    update

--- Pytorch_UNet_ticks_C_R/op.py
+++ Pytorch_UNet_ticks_C_R/op.py
@@ -273,6 +273,7 @@ class Vector_Regression_Loss(nn.Module):
 
 
                             loss += (((rs_vector[0]-gt_vector[0])*1.)**2 + ((rs_vector[1]-gt_vector[1])*1.)**2)**0.5
+                            print(loss)
                     if _class == 4:
                         if gt[k,i,j,0] == 4:
                             overlap_area += 1
@@ -281,6 +282,7 @@ class Vector_Regression_Loss(nn.Module):
 
 
                             loss += (((rs_vector[0]-gt_vector[0])*1.)**2 + ((rs_vector[1]-gt_vector[1])*1.)**2)**0.5
+                            print(loss)
 
         
         loss /= overlap_area

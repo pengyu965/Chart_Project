a6ebe42f37d253dc72c415ff4a3c3424a4920f12
pengyu965 <py.yan965@gmail.com>
2019-08-01 18:44:21 -0400

    update

--- Pytorch_UNet_ticks_C_R/op.py
+++ Pytorch_UNet_ticks_C_R/op.py
@@ -276,7 +276,7 @@ class Vector_Regression_Loss(nn.Module):
                             rs_vector = result[k,6:,i,j]
 
 
-                            loss += np.linalg.norm((rs_vector[0]-gt_vector[0],rs_vector[1]-gt_vector[1]))
+                            loss += (((rs_vector[0]-gt_vector[0])*1.)**2,((rs_vector[1]-gt_vector[1])*1.)**2)**0.5
         
         loss /= overlap_area
         return loss

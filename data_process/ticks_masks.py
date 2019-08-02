41ea5ef21c2ab60b9322467a28d4b3364091547b
pengyu965 <py.yan965@gmail.com>
2019-08-02 15:07:22 -0400

    update

--- data_process/ticks_masks.py
+++ data_process/ticks_masks.py
@@ -143,18 +143,22 @@ def masks_gen(gt_json):
             class_arr[i,j] = idi
 
     # Process the vector layer
+    a = np.zeros((512,512,3)).astype(np.uint8)
     for i in range(x):
         for j in range(y):
             if vector_center_masks[i,j,0] != 0 or vector_center_masks[i,j,1] != 0:
                 vector = vector_center_masks[i,j,:] - np.array([i,j])
+                print(vector_center_masks[i,j,:])
 
                 # Need to process the center, where vector = 0,0
                 # if vector[0] == 0 and vector[1] == 0:
                     # vector_masks[i,j,:] = vector
                 # else:
                 #     vector_masks[i,j,:] = vector/((vector[0]**2+vector[1]**2)**0.5)
-                a = vector_masks[:,:,0]
-                cv2.circle(a, tuple(vector_center_masks[i,j,:].astype(np.int)),5,(255), -1)
+
+                cv2.circle(a, (j,i),5, (255,255,0),-1)
+                # cv2.circle(a, tuple(vector_center_masks[i,j,:].astype(np.int)),5,(255,255,255), -1)
+                # cv2.arrowedLine(a, (i,j), (int(i+vector[0]),int(j+vector[1])),(255,0,0),1)
 
     cv2.imshow("example", a)
     cv2.waitKey(0)

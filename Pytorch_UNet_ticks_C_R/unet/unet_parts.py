2934e6dc5c1ef8fc9521f04402715caed731a7e3
pengyu965 <py.yan965@gmail.com>
2019-08-05 21:19:29 -0400

    update

--- Pytorch_UNet_ticks_C_R/unet/unet_parts.py
+++ Pytorch_UNet_ticks_C_R/unet/unet_parts.py
@@ -11,11 +11,13 @@ class double_conv(nn.Module):
         super(double_conv, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True),
+            # nn.BatchNorm2d(out_ch),
+            # nn.ReLU(inplace=True),
+            nn.LeakyReLU(1, True),
             nn.Conv2d(out_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True)
+            # nn.BatchNorm2d(out_ch),
+            # nn.ReLU(inplace=True)
+            nn.LeakyReLU(1, True)
         )
 
     def forward(self, x):

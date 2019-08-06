37a693617896e7ece6376f3d12b43c7485b56acd
pengyu965 <py.yan965@gmail.com>
2019-08-05 22:31:11 -0400

    update

--- Pytorch_UNet_ticks_C_R/test.py
+++ Pytorch_UNet_ticks_C_R/test.py
@@ -5,14 +5,16 @@ import os
 a = np.load("../../data/SUMIT/rs_masks_sampled/545.npy")
 b = np.zeros((512,512,3))
 x,y,z = a.shape
-
+length = []
 for i in range(x):
     for j in range(y):
         if a[j,i,1] != 0 or a[j,i,2] != 0:
             # cv2.circle(b, (int(i+a[j,i,1]),int(j+a[j,i,2])),3,(255,255,255),-1)
+            length.append(np.sqrt(a[j,i,1]**2+a[j,i,2]**2))
 
-            print(a[j,i,1:])
+print(np.max(np.array(length)))
 
 
-cv2.imshow("example", b)
-cv2.waitKey(0)
+
+# cv2.imshow("example", b)
+# cv2.waitKey(0)
--- Pytorch_UNet_ticks_C_R/unet/unet_parts.py
+++ Pytorch_UNet_ticks_C_R/unet/unet_parts.py
@@ -11,13 +11,11 @@ class double_conv(nn.Module):
         super(double_conv, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
-            # nn.BatchNorm2d(out_ch),
-            # nn.ReLU(inplace=True),
-            nn.LeakyReLU(1, True),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
             nn.Conv2d(out_ch, out_ch, 3, padding=1),
-            # nn.BatchNorm2d(out_ch),
-            # nn.ReLU(inplace=True)
-            nn.LeakyReLU(1, True)
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
         )
 
     def forward(self, x):

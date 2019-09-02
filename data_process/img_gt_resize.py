e16a8cf9a6972f54c53d3e3c4127d2c2222cd376
pengyu965 <py.yan965@gmail.com>
2019-09-02 10:40:41 -0400

    update

--- data_process/img_gt_resize.py
+++ data_process/img_gt_resize.py
@@ -20,38 +20,39 @@ if os.path.exists(rs_gt_path) == False:
 def ticks_gt_gen(file_name):
     img = cv2.imread(img_path+file_name+".png")
     h,w,c = img.shape
-    rs_img = cv2.resize(img, (512,512), interpolation=cv2.INTER_AREA)
+    rs_w, rs_h = 512,512
+    rs_img = cv2.resize(img, (rs_w, rs_h), interpolation=cv2.INTER_AREA)
     gt_dic = json.load(open(gt_path + file_name+".json",'r'))
     # needed_dic = gt_dic["task6"]
     needed_dic = gt_dic
     for text_bb in needed_dic["input"]["task2_output"]["text_blocks"]:
         for item in text_bb["bb"]:
             if item == "height" or item =="y0":
-                text_bb["bb"][item] = round(text_bb["bb"][item] * 512/h)
+                text_bb["bb"][item] = round(text_bb["bb"][item] * rs_h/h)
             if item == "width" or item =="x0":
-                text_bb["bb"][item] = round(text_bb["bb"][item] * 512/w)
+                text_bb["bb"][item] = round(text_bb["bb"][item] * rs_w/w)
 
     #     cv2.rectangle(img, (text_bb["bb"]["x0"], text_bb["bb"]["y0"]), (text_bb["bb"]["x0"]+text_bb["bb"]["width"],text_bb["bb"]["y0"]+text_bb["bb"]["height"]), [0,0,255],2)
     #     cv2.putText(img, str(text_bb["id"]),(text_bb["bb"]["x0"], text_bb["bb"]["y0"]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0))
     # cv2.imshow("example",img)
     # cv2.waitKey(0)
     # print(json.dumps(needed_dic, indent=4))
-    needed_dic["input"]["task4_output"]["_plot_bb"]["height"] = round(needed_dic["input"]["task4_output"]["_plot_bb"]["height"]*512/h)
-    needed_dic["input"]["task4_output"]["_plot_bb"]["y0"] = round(needed_dic["input"]["task4_output"]["_plot_bb"]["y0"]*512/h)
-    needed_dic["input"]["task4_output"]["_plot_bb"]["x0"] = round(needed_dic["input"]["task4_output"]["_plot_bb"]["x0"]*512/w)
-    needed_dic["input"]["task4_output"]["_plot_bb"]["width"] = round(needed_dic["input"]["task4_output"]["_plot_bb"]["width"]*512/w)
+    needed_dic["input"]["task4_output"]["_plot_bb"]["height"] = round(needed_dic["input"]["task4_output"]["_plot_bb"]["height"]*rs_h/h)
+    needed_dic["input"]["task4_output"]["_plot_bb"]["y0"] = round(needed_dic["input"]["task4_output"]["_plot_bb"]["y0"]*rs_h/h)
+    needed_dic["input"]["task4_output"]["_plot_bb"]["x0"] = round(needed_dic["input"]["task4_output"]["_plot_bb"]["x0"]*rs_w/w)
+    needed_dic["input"]["task4_output"]["_plot_bb"]["width"] = round(needed_dic["input"]["task4_output"]["_plot_bb"]["width"]*rs_w/w)
     for axis in needed_dic["input"]["task4_output"]["axes"]:
         for sub_dic in needed_dic["input"]["task4_output"]["axes"][axis]:
-            sub_dic["tick_pt"]["x"] = round(sub_dic["tick_pt"]["x"]*512/w)
-            sub_dic["tick_pt"]["y"] = round(sub_dic["tick_pt"]["y"]*512/h)
+            sub_dic["tick_pt"]["x"] = round(sub_dic["tick_pt"]["x"]*rs_w/w)
+            sub_dic["tick_pt"]["y"] = round(sub_dic["tick_pt"]["y"]*rs_h/h)
 
     for lengend_pair in needed_dic["input"]["task5_output"]:
         for text_bb in needed_dic["input"]["task5_output"][lengend_pair]:
             for item in text_bb["bb"]:
                 if item == "height" or item =="y0":
-                    text_bb["bb"][item] = round(text_bb["bb"][item] * 512/h)
+                    text_bb["bb"][item] = round(text_bb["bb"][item] * rs_h/h)
                 if item == "width" or item =="x0":
-                    text_bb["bb"][item] = round(text_bb["bb"][item] * 512/w)
+                    text_bb["bb"][item] = round(text_bb["bb"][item] * rs_w/w)
 
 
     cv2.imwrite(rs_img_path+file_name+".png", rs_img)

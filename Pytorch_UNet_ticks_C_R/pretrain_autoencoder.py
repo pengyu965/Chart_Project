94b3c57dc3e910b61f99adecef6e90d4f5c2612a
pengyu965 <py.yan965@gmail.com>
2019-09-14 16:22:47 -0400

    update

--- Pytorch_UNet_ticks_C_R/pretrain_autoencoder.py
+++ Pytorch_UNet_ticks_C_R/pretrain_autoencoder.py
@@ -10,8 +10,74 @@ import time
 import cv2
 from PIL import Image
 import numpy as np
-from unet.unet_parts import *
-from util import *
+
+class double_conv(nn.Module):
+    '''(conv => BN => ReLU) * 2'''
+    def __init__(self, in_ch, out_ch):
+        super(double_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class inconv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(inconv, self).__init__()
+        self.conv = double_conv(in_ch, out_ch)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class down(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(down, self).__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool2d(2),
+            double_conv(in_ch, out_ch)
+        )
+
+    def forward(self, x):
+        x = self.mpconv(x)
+        return x
+
+class outconv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(outconv, self).__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, 1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class up1(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear=False):
+        super(up1, self).__init__()
+
+        #  would be a nice idea if the upsampling could be learned too,
+        #  but my machine do not have enough memory to handle all those weights
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
+
+        self.conv = double_conv(in_ch, out_ch)
+
+    def forward(self, x1):
+        x1 = self.up(x1)
+        
+        x = self.conv(x1)
+        return x
 
 
 class UNet(nn.Module):
@@ -22,11 +88,11 @@ class UNet(nn.Module):
         self.down2 = down(128, 256)
         self.down3 = down(256, 512)
         self.down4 = down(512, 512)
-        self.up11 = nn.ConvTranspose2d(512, 256, 2, stride=2)
-        self.up12 = nn.ConvTranspose2d(256, 128, 2, stride=2)
-        self.up13 = nn.ConvTranspose2d(128, 64, 2, stride=2)
-        self.up14 = nn.ConvTranspose2d(64, 32, 2, stride=2)
-        self.out1 = outconv(32, out_channels)
+        self.up1 = up1(1024, 256)
+        self.up2 = up1(512, 128)
+        self.up3 = up1(256, 64)
+        self.up4 = up1(128, 64)
+        self.outc = outconv(64, out_channels)
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -34,11 +100,11 @@ class UNet(nn.Module):
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-        x = self.up11(x5)
-        x = self.up12(x)
-        x = self.up13(x)
-        x = self.up14(x)
-        x = self.out1(x)
+        x = self.up1(x5)
+        x = self.up2(x)
+        x = self.up3(x)
+        x = self.up4(x)
+        x = self.outc(x)
         # out = torch.tanh(x)
         out = x 
         return out

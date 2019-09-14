5eed847f84fe8ee209145c81a6ae625802dd9e01
pengyu965 <py.yan965@gmail.com>
2019-09-14 17:10:09 -0400

    update

--- Pytorch_UNet_ticks_C_R/pretrain_autoencoder.py
+++ Pytorch_UNet_ticks_C_R/pretrain_autoencoder.py
@@ -11,104 +11,104 @@ import cv2
 from PIL import Image
 import numpy as np
 from util import *
-from unet import UNet
+# from unet import UNet
 
-# class double_conv(nn.Module):
-#     '''(conv => BN => ReLU) * 2'''
-#     def __init__(self, in_ch, out_ch):
-#         super(double_conv, self).__init__()
-#         self.conv = nn.Sequential(
-#             nn.Conv2d(in_ch, out_ch, 3, padding=1),
-#             nn.BatchNorm2d(out_ch),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(out_ch, out_ch, 3, padding=1),
-#             nn.BatchNorm2d(out_ch),
-#             nn.ReLU(inplace=True)
-#         )
+class double_conv(nn.Module):
+    '''(conv => BN => ReLU) * 2'''
+    def __init__(self, in_ch, out_ch):
+        super(double_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
 
-#     def forward(self, x):
-#         x = self.conv(x)
-#         return x
-
-
-# class inconv(nn.Module):
-#     def __init__(self, in_ch, out_ch):
-#         super(inconv, self).__init__()
-#         self.conv = double_conv(in_ch, out_ch)
-
-#     def forward(self, x):
-#         x = self.conv(x)
-#         return x
+    def forward(self, x):
+        x = self.conv(x)
+        return x
 
 
-# class down(nn.Module):
-#     def __init__(self, in_ch, out_ch):
-#         super(down, self).__init__()
-#         self.mpconv = nn.Sequential(
-#             nn.MaxPool2d(2),
-#             double_conv(in_ch, out_ch)
-#         )
+class inconv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(inconv, self).__init__()
+        self.conv = double_conv(in_ch, out_ch)
 
-#     def forward(self, x):
-#         x = self.mpconv(x)
-#         return x
+    def forward(self, x):
+        x = self.conv(x)
+        return x
 
-# class outconv(nn.Module):
-#     def __init__(self, in_ch, out_ch):
-#         super(outconv, self).__init__()
-#         self.conv = nn.Conv2d(in_ch, out_ch, 1)
 
-#     def forward(self, x):
-#         x = self.conv(x)
-#         return x
+class down(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(down, self).__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool2d(2),
+            double_conv(in_ch, out_ch)
+        )
 
-# class up1(nn.Module):
-#     def __init__(self, in_ch, out_ch, bilinear=True):
-#         super(up1, self).__init__()
+    def forward(self, x):
+        x = self.mpconv(x)
+        return x
 
-#         #  would be a nice idea if the upsampling could be learned too,
-#         #  but my machine do not have enough memory to handle all those weights
-#         if bilinear:
-#             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-#         else:
-#             self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
+class outconv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(outconv, self).__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, 1)
 
-#         self.conv = double_conv(in_ch//2, out_ch)
+    def forward(self, x):
+        x = self.conv(x)
+        return x
 
-#     def forward(self, x1):
-#         x1 = self.up(x1)
+class up1(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear=False):
+        super(up1, self).__init__()
+
+        #  would be a nice idea if the upsampling could be learned too,
+        #  but my machine do not have enough memory to handle all those weights
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
+
+        self.conv = double_conv(in_ch//2, out_ch)
+
+    def forward(self, x1):
+        x1 = self.up(x1)
         
-#         x = self.conv(x1)
-#         return x
+        x = self.conv(x1)
+        return x
 
 
-# class UNet(nn.Module):
-#     def __init__(self, in_channels, out_channels):
-#         super(UNet, self).__init__()
-#         self.inc = inconv(in_channels, 64)
-#         self.down1 = down(64, 128)
-#         self.down2 = down(128, 256)
-#         self.down3 = down(256, 512)
-#         self.down4 = down(512, 512)
-#         self.up1 = up1(1024, 256)
-#         self.up2 = up1(512, 128)
-#         self.up3 = up1(256, 64)
-#         self.up4 = up1(128, 64)
-#         self.outc = outconv(64, out_channels)
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNet, self).__init__()
+        self.inc = inconv(in_channels, 64)
+        self.down1 = down(64, 128)
+        self.down2 = down(128, 256)
+        self.down3 = down(256, 512)
+        self.down4 = down(512, 512)
+        self.up1 = up1(1024, 256)
+        self.up2 = up1(512, 128)
+        self.up3 = up1(256, 64)
+        self.up4 = up1(128, 64)
+        self.outc = outconv(64, out_channels)
 
-#     def forward(self, x):
-#         x1 = self.inc(x)
-#         x2 = self.down1(x1)
-#         x3 = self.down2(x2)
-#         x4 = self.down3(x3)
-#         x5 = self.down4(x4)
-#         x = self.up1(x5)
-#         x = self.up2(x)
-#         x = self.up3(x)
-#         x = self.up4(x)
-#         x = self.outc(x)
-#         out = torch.tanh(x)
-#         return out
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5)
+        x = self.up2(x)
+        x = self.up3(x)
+        x = self.up4(x)
+        x = self.outc(x)
+        out = torch.tanh(x)
+        return out
 
 class Chartdata(Dataset):
     def __init__(self, img_path):
@@ -129,7 +129,7 @@ model = nn.DataParallel(UNet(3,3)).to(device)
 print(model) 
 
 batch_size = 12
-lr = 0.0001
+lr = 0.001
 epoch = 20
 
 optimizer = optim.Adam(model.parameters(), lr = lr, weight_decay=1e-5)
@@ -178,7 +178,7 @@ for ep in range(epoch):
             for i in range(0,5121-5120//nroll,5120//nroll):
                 try:
                     for j in range(0, 5121-5120//nroll,5120//nroll):
-                        im = Image.fromarray(image_norm(output_images[index].permute(1,2,0).detach().cpu().clone().numpy()).astype("uint8"))
+                        im = Image.fromarray(image_norm(train_images[index]*1./255.permute(1,2,0).detach().cpu().clone().numpy()).astype("uint8"))
                         im.thumbnail((512,512))
                         new_im.paste(im, (i,j))
                         print(index)

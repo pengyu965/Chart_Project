df46cf49d6d6e6dddcf07cee70be431670e62b25
pengyu965 <py.yan965@gmail.com>
2019-09-15 01:34:11 -0400

    update

--- Pytorch_UNet_ticks_C_R/pretrain_autoencoder.py
+++ Pytorch_UNet_ticks_C_R/pretrain_autoencoder.py
@@ -172,7 +172,7 @@ for ep in range(epoch):
         loss.backward
         optimizer.step()
 
-        grad_v = model.module.down1.mpconv
+        grad_v = model.module.down1.mpconv.grad
         print(grad_v)
 
         print("Epoch:[{}]===Step:[{}/{}]===Time:[{:.2f}]===Learning Rate:{}===Regression_Loss:[{:.4f}]".format(ep, idi, idx, time.time()-start_time, lr, loss.item()))

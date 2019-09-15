db1318990cf41e6ef91aab58534f2dc7e23e173f
pengyu965 <py.yan965@gmail.com>
2019-09-15 01:56:36 -0400

    update

--- Pytorch_UNet_ticks_C_R/pretrain_autoencoder.py
+++ Pytorch_UNet_ticks_C_R/pretrain_autoencoder.py
@@ -167,7 +167,7 @@ for ep in range(epoch):
 
         # print(output_images)
 
-        loss = criterion(output_images, train_images)
+        loss = criterion(output_images, train_images*1./255)
 
         loss.backward()
         optimizer.step()
@@ -181,7 +181,7 @@ for ep in range(epoch):
             for i in range(0,5121-5120//nroll,5120//nroll):
                 try:
                     for j in range(0, 5121-5120//nroll,5120//nroll):
-                        im = Image.fromarray(image_norm(1./255*output_images[index].permute(1,2,0).detach().cpu().clone().numpy()).astype("uint8"))
+                        im = Image.fromarray(image_norm(output_images[index].permute(1,2,0).detach().cpu().clone().numpy()).astype("uint8"))
                         im.thumbnail((512,512))
                         new_im.paste(im, (i,j))
                         print(index)

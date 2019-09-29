e5103979279a4047dc697ba6512823512f9c96a4
pengyu965 <py.yan965@gmail.com>
2019-09-29 16:50:24 -0400

    update

--- Pytorch_UNet_ticks_C_R/op.py
+++ Pytorch_UNet_ticks_C_R/op.py
@@ -24,7 +24,7 @@ def weights_init(m):
 
 class Operator:
     def __init__(self, netG, netD = None):
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
         self.netG = nn.DataParallel(netG, [1,2,3]).to(self.device)
         self.netD = netD
         self.criterion = nn.CrossEntropyLoss()

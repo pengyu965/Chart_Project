84eaa5dc148e01c090dbf1b6d2bdb628b7f8c819
pengyu965 <py.yan965@gmail.com>
2019-10-04 15:01:50 -0400

    update

--- Pytorch_UNet_ticks_C_R/op.py
+++ Pytorch_UNet_ticks_C_R/op.py
@@ -27,7 +27,8 @@ class Operator:
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.netG = nn.DataParallel(netG).to(self.device)
         self.netD = netD
-        self.criterion = nn.CrossEntropyLoss(weight=torch.tensor([0.15,0.25,0.15,0.25,0.15,0.05]).to(self.device))
+        # self.criterion = nn.CrossEntropyLoss(weight=torch.tensor([0.15,0.25,0.15,0.25,0.15,0.05]).to(self.device))
+        self.criterion = nn.CrossEntropyLoss(weight=torch.tensor([15,25,15,55,5,1]).to(self.device))
         self.criterion_r = Vector_Regression_Loss()
         self.loss_coefficent = 50.0
 
